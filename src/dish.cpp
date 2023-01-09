233ec7191948aaed481c0a8185d35460c62c8c1b
aleferna12 <aleferna2001@hotmail.com>
2023-01-09 14:12:24 +0000

    Removed nlohmann dependency

--- src/dish.cpp
+++ src/dish.cpp
@@ -30,7 +30,6 @@ Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 #include <cstring>
 #include <iostream>
 #include <cmath>
-#include <nlohmann/json.hpp>
 #include "dish.h"
 #include "sticky.h"
 #include "crash.h"
